84f3abd36a2d192202896a9fe37bed4c2e9575e0
mohitmeena2107 <manumeherofficial@gmail.com>
2025-08-27 17:28:52 +0530

    import issue fixed

--- server/models/User.js
+++ server/models/User.js
@@ -16,5 +16,6 @@ const userSchema = new Schema(
   },
   { timestamps: true, minimize: false }
 );
+const User = mongoose.model("User", userSchema);
+export default User;
 
-export default User = mongoose.model("User", userSchema);
