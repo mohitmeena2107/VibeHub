7869156cc7edb39be3828c623a7e959c44385dd1
mohitmeena2107 <manumeherofficial@gmail.com>
2025-08-27 17:25:31 +0530

    import issue fixed

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import {protect} from "../middlewares/auth.js"
 import {upload} from "../config/multer.js"
-import { getChatMessages, sendMessage, sseController } from "../controllers/messageController"
+import { getChatMessages, sendMessage, sseController } from "../controllers/messageController.js"
 
 
 const messageRouter = express.Router()
