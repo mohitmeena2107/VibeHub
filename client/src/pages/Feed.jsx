e467f92d270e63752287bad793ef59edb784fcea
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2025-08-27 18:19:08 +0530

    added story bar

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Loading } from '../components/Loading';
+import StoryBar from '../components/StoryBar';
+import {dummyUserData} from '../assets/assets' 
+
 
 const Feed = () => {
-  return (
-    <div>ohho</div>
-  )
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchFeed = async () => {
+    setFeeds(dummyUserData);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchFeed();
+  },[])
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar xl:pr-5 py-10 flex items-start justify-center xl:gap-8'>
+      {/* Stories and post list */}
+      <div>
+        <StoryBar/>
+        <div className='p-4 space-y-6'>List of post</div>
+      </div>
+      {/* Right Side */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+        <h1>Recent messages</h1>
+      </div>
+    </div>
+  ) : <Loading />
 }
 
 export default Feed
